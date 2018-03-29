8ccb99864d9e8ea8d858dbe6239f6130b6f48055
Cristian-Dan Bara <cristian.bara@braintribe.com>
2018-03-29 20:32:22 +0200

    more experiments

--- experiments/pain/drawTimelineCorrelation.js
+++ experiments/pain/drawTimelineCorrelation.js
@@ -59,7 +59,7 @@ function drawTable(keywords) {
 
 function drawChart(keywords) {
 
-    var requestString = 'https://trendydots-api-trendydots.193b.starter-ca-central-1.openshiftapps.com/googleCorrelateInterestOverTime?'
+    var requestString = 'http://trendydots-api-trendydots.193b.starter-ca-central-1.openshiftapps.com/googleCorrelateInterestOverTime?'
     // set keywords
     for (var i = 0; i < keywords.length; i++) {
         if(keywords[i].mid){
